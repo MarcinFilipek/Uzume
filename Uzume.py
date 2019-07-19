dd0451e97aad93324804d3148236b4987b81a3b1
Marcin <filipekmarcin180@gmail.com>
2019-07-19 18:07:06 +0200

    name serial port move to env variable

--- Uzume.py
+++ Uzume.py
@@ -1,11 +1,12 @@
 from NucleoExpander import NucleoExpander
+import os
 
 
 def main():
     print("Uzume: Hello.")
-    nucleo = NucleoExpander('COM10', 10)
-    nucleo.horizontal_servo_set_angle(60)
-    nucleo.vertical_servo_set_angle(57)
+    nucleo = NucleoExpander(os.environ['SERIAL_PORT'], 10)
+    nucleo.horizontal_servo_set_angle(40)
+    nucleo.vertical_servo_set_angle(150)
     nucleo.packetSerial.send_frame()
     print("Uzume: Bye.")
 
